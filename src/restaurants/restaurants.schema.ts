1282daf0bba6ec77a3bd10a958a9dbddb36f1ea3
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2026-05-21 05:43:28 +0300

    Update(Restaurant Schema): add location geoJson point

--- src/restaurants/restaurants.schema.ts
+++ src/restaurants/restaurants.schema.ts
@@ -33,6 +33,18 @@ export class Restaurant {
     })
     cuisines!: string[];
 
+    @Prop({
+        type: { type: String, enum: ['Point'], required: true },
+        coordinates: { type: [Number], required: true }  
+    })
+    location!: {
+        type: 'Point';
+        coordinates: [number, number]; // [long, lat]  
+    };
 }
 
+
 export const RestaurantSchema = SchemaFactory.createForClass(Restaurant);
+
+// 2dsphere to support query like (near, within)
+RestaurantSchema.index({ location: '2dsphere' });
